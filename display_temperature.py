a14daffe50fbb031951f9f685dd3be4562ea510e
pedro-in-liny <plmariano@fc.ul.pt>
2018-09-07 17:41:49 +0000

    Working!!!

--- display_temperature.py
+++ display_temperature.py
@@ -16,7 +16,7 @@ from PySide import QtGui, QtCore
 
 import assisipy
 
-import inter_domset.ISI.consumer_IB
+import consumer_IB
 
 MIN_TEMPERATURE = 26
 MAX_TEMPERATURE = 39
@@ -30,7 +30,7 @@ class Example(QtGui.QWidget):
             a_node : 29
             for a_node in list_nodes
         }
-        self.node_listener = inter_domset.ISI.consumer_IB.BeeArenaListener (proj_conf, path, verb = False, logfile = None)
+        self.node_listener = consumer_IB.BeeArenaListener (proj_conf, path, verb = False, logfile = None)
         self.node_listener.start_rx ()
         # self.dict_casus_interface = {
         #     a_casu_number : assisipy.casu (rtc_file_name = a_rtc_filename, log = False)
@@ -41,26 +41,26 @@ class Example(QtGui.QWidget):
         self.timer.start (SPEED, self)
         
     def initUI(self):      
-        self.setGeometry(0, 0, 600, 200)
+        self.setGeometry(0, 0, 1200, 800)
         self.setWindowTitle('CASU temperature')
         self.show()
 
     def paintEvent(self, event):
         qp = QtGui.QPainter()
         qp.begin(self)
-        self.drawNode (event, qp, 'A', 50, 50)
-        self.drawNode (event, qp, 'B', 250, 50)
-        self.drawNode (event, qp, 'C', 450, 50)
+        self.drawNode (event, qp, 'A', 1190, 580, 100, 100)
+        self.drawNode (event, qp, 'B', 960, 340, 300, 140)
+        self.drawNode (event, qp, 'C', 740, 375, 100, 100)
 #        self.drawNode (event, qp, 'casu-053', 50, 50)
 #        self.drawNode (event, qp, 'casu-054', 250, 50)
 #        self.drawNode (event, qp, 'casu-048', 450, 50)
         qp.end()
 
-    def keyPressEvent (self, event):
-        if event is not None:
-            print ('Key event {}'.format (event))
-            self.timer.stop ()
-            sys.exit (0)
+#    def keyPressEvent (self, event):
+#        if event is not None:
+#            print ('Key event {}'.format (event))
+#            self.timer.stop ()
+#            sys.exit (0)
 
     def update_casu_temp (self):
         # for casu_number, casu_interface in self.dict_casus_interface:
@@ -81,7 +81,7 @@ class Example(QtGui.QWidget):
         self.update_casu_temp ()
         self.update ()
         
-    def drawNode (self, event, qp, casu, x, y):
+    def drawNode (self, event, qp, casu, x, y, w, h):
         print ('Drawing casu {}'.format (casu))
         temperature = self.temp_data [casu]
         if temperature >= MIN_TEMPERATURE and temperature <= MAX_TEMPERATURE:
@@ -89,16 +89,16 @@ class Example(QtGui.QWidget):
             if temp_relative < 0.5:
                 color = QtGui.QColor (0, 32, 255 - int (temp_relative * 255))
             else:
-                color = QtGui.QColor (int ((temp_relative - 0.5 * 255)), 32, 0)
+                color = QtGui.QColor (int ((temp_relative - 0.5) * 255), 32, 0)
             pen = QtGui.QPen (color)
             pen.setWidth (10)
             qp.setPen (pen)
-            qp.drawEllipse (x, y, 100, 100)
+            qp.drawEllipse (x, y, w, h)
                        
 def main():
     app = QtGui.QApplication(sys.argv)
     print (sys.argv)
-    ex = Example(['A', 'B', 'C'], '/home/assisi/assisi/pedro/ARS18/cfgs/1-line3/1-line3.conf', '/home/assisi/assisi/pedro/ARS18/cfgs/1-line3/')
+    ex = Example(['A', 'B', 'C'], '/home/pi/code/cfgs/1-line3/1-line3.conf', '/home/pi/code/cfgs/1-line3/')
 #    ex = Example(['casu-053', 'casu-048', 'casu-054'], '/home/assisi/assisi/pedro/ARS18/cfgs/1-line3/1-line3.conf', '/home/assisi/assisi/pedro/ARS18/cfgs/1-line3/')
     sys.exit(app.exec_())
 
